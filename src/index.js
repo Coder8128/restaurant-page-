49848b08d42583f09a2b076123b145373c741399
rahat <57500825+Coder8128@users.noreply.github.com>
2026-02-15 16:46:25 +0100

    Add image

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@
 export const greeting = "Hello, Odinite!";
 
 console.log(greeting);
+
+// src/index.js
+import odinImage from "./food.png";
+
+const image = document.createElement("img");
+image.src = odinImage;
+image.classList.add('food-image');
+
+document.querySelector(".img").appendChild(image);
